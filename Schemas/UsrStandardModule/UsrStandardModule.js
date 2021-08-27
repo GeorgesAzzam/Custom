fc01c1715fd985a563e69c4e94d1cdd3e88b3dfe
georges <georges.azzam@hotmail.com>
2021-08-27 21:42:33 +0300

    first commit

---
+++ Schemas/UsrStandardModule/UsrStandardModule.js
@@ -0,0 +1,9 @@
+define("UsrStandardModule", [],
+    function () {
+        return {
+            config:{},
+            init: function () {
+                this.config.prop1 = 1;
+            }
+        };
+    });
